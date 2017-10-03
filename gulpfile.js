58920c572289a657fabe5736394c5b5f283fd03d
Rich Donohue <rgdonohue@gmail.com>
2017-10-03 08:40:54 -0600

    update src and dist with docs

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,7 @@ gulp.task('scripts', function() {
 gulp.task('copy', function() {
     gulp.src(['./src/css/**/*']).pipe(gulp.dest('./dist/css'));
     gulp.src(['./src/data/*']).pipe(gulp.dest('./dist/data'));
+    gulp.src(['./src/docs/*']).pipe(gulp.dest('./dist/docs'));
     gulp.src(['./src/images/*']).pipe(gulp.dest('./dist/images'));
     gulp.src(['./src/index.html']).pipe(gulp.dest('./dist/'));
     gulp.src(['./src/CNAME']).pipe(gulp.dest('./dist/'));
